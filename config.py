e3fb8ee3856a4791e57a5096b629e0339daa2121
coded <55897154+TeamAbabilCoded@users.noreply.github.com>
2023-07-29 18:13:51 +0700

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ API_HASH = os.environ.get("API_HASH", "091fb5d93efbf3fb34413d982a0cf617")
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001704111351"))
 
 # NAMA OWNER
-OWNER = os.environ.get("OWNER", "r4xaa")
+OWNER = os.environ.get("OWNER", "t_ruz")
 
 # Protect Content
 PROTECT_CONTENT = strtobool(os.environ.get("PROTECT_CONTENT", "False"))
